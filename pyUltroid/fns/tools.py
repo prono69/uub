a51480bae4037525e4303b855b393c667f8750fe
lanowde <117498666+lanowde@users.noreply.github.com>
2023-03-02 17:07:01 +0530

    r3k1

--- pyUltroid/fns/tools.py
+++ pyUltroid/fns/tools.py
@@ -69,6 +69,7 @@ try:
 except ImportError:
     BeautifulSoup = None
 
+
 # ~~~~~~~~~~~~~~~~~~~~OFOX API~~~~~~~~~~~~~~~~~~~~
 # @buddhhu
 
@@ -82,44 +83,6 @@ async def get_ofox(codename):
         ofox_baseurl + "devices/get?codename=" + codename, re_json=True
     )
     return device, releases
-
-
-# ~~~~~~~~~~~~~~~Async Searcher~~~~~~~~~~~~~~~
-# @buddhhu
-
-
-async def async_searcher(
-    url: str,
-    post: bool = None,
-    headers: dict = None,
-    params: dict = None,
-    json: dict = None,
-    data: dict = None,
-    ssl=None,
-    re_json: bool = False,
-    re_content: bool = False,
-    real: bool = False,
-    *args,
-    **kwargs,
-):
-    if not aiohttp:
-        raise DependencyMissingError(
-            "'aiohttp' is not installed!\nthis function requires aiohttp to be installed."
-        )
-    async with aiohttp.ClientSession(headers=headers) as client:
-        if post:
-            data = await client.post(
-                url, json=json, data=data, ssl=ssl, *args, **kwargs
-            )
-        else:
-            data = await client.get(url, params=params, ssl=ssl, *args, **kwargs)
-        if re_json:
-            return await data.json()
-        if re_content:
-            return await data.read()
-        if real:
-            return data
-        return await data.text()
 
 
 # ~~~~~~~~~~~~~~~JSON Parser~~~~~~~~~~~~~~~
