35c2fadf47a803e293daab21f838bc46004ae233
lanowde <117498666+lanowde@users.noreply.github.com>
2023-04-14 19:37:59 +0530

    mini

--- plugins/_help.py
+++ plugins/_help.py
@@ -15,7 +15,7 @@ from telethon.tl.custom import Button
 from pyUltroid.dB._core import HELP, LIST
 from pyUltroid.fns.tools import cmd_regex_replace
 
-from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, inline_pic, udB, ultroid_cmd
+from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, inline_pic, random_pic, udB, ultroid_cmd
 
 
 _main_help_menu = [
