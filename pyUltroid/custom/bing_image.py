7cea8551c2100aaf39912bc43ba8e52aa13c1c9a
lanowde <117498666+lanowde@users.noreply.github.com>
2023-06-29 11:47:24 +0530

    fix bing img and requirements.txt
    - removed aiofiles
    - optional spotdl, imgurpy, pygithub

--- pyUltroid/custom/bing_image.py
+++ pyUltroid/custom/bing_image.py
@@ -3,10 +3,13 @@
 
 import asyncio
 import imghdr
+from functools import partial
 from pathlib import Path
 from random import choice, shuffle
-from re import findall
-from urllib.parse import quote_plus
+from re import match, search, findall
+from urllib.parse import quote_plus, unquote
+
+import aiohttp
 
 from .. import LOGS
 from ..fns import some_random_headers
@@ -19,6 +22,9 @@ from ..fns.helper import (
 )
 
 
+_IMG_EXTS = (".jpg", ".jpeg", ".exif", ".gif", ".bmp", ".png", ".webp", ".jpe", ".tiff")
+
+
 class BingScrapper:
     def __init__(self, query, limit, hide_nsfw=True, filter=None):
         assert bool(query), "No query provided.."
@@ -27,10 +33,7 @@ class BingScrapper:
         self.limit = limit
         self.page_counter = 0
         self.hide_nsfw = "on" if bool(hide_nsfw) else "off"
-        self.url_args = self.filter_to_args(filter)
-        self._image_ext = (
-            ".jpg .jpeg .jfif .exif .gif .bmp .png .webp .jpe .tiff".split()
-        )
+        self.url_args = self._filter_to_args(filter)
         self.output_path = check_filename(f"resources/downloads/bing-{query}")
         self.headers = {
             "User-Agent": choice(some_random_headers),
@@ -41,7 +44,7 @@ class BingScrapper:
             "Connection": "keep-alive",
         }
 
-    def filter_to_args(self, shorthand):
+    def _filter_to_args(self, shorthand):
         if not shorthand:
             return ""
         shorthand = shorthand.lower()
@@ -63,17 +66,31 @@ class BingScrapper:
         with open(filename, "wb+") as f:
             f.write(content)
 
-    async def save_image(self, link):
-        filename = _get_filename_from_url(link, self.output_path)
-        ext = Path(filename).suffix
-        if not (ext and ext in self._image_ext):
-            filename += ".jpg"
-        try:
-            image_data = await async_searcher(link, re_content=True)
+    async def _handle_request(self, filename, response):
+        if response.status < 207:
+            image_data = await response.read()
             if imghdr.what(None, image_data):
                 await self._save_to_file(filename, image_data)
+
+    async def save_image(self, link):
+        if match(r"^https?://(www.)?bing.com/th/id/OGC", link):
+            if re_search := search(r"&amp;rurl=(.+)&amp;ehk=", link):
+                link = unquote(re_search.group(1))
+        filename = _get_filename_from_url(link, self.output_path)
+        ext = Path(filename).suffix
+        if not (ext and ext in _IMG_EXTS):
+            filename += ".jpg"
+        try:
+            await async_searcher(
+                link,
+                raise_for_status=True,
+                timeout=aiohttp.ClientTimeout(total=8),
+                evaluate=partial(self._handle_request, filename),
+            )
+        except (asyncio.TimeoutError, aiohttp.ClientResponseError):
+            pass
         except Exception as exc:
-            LOGS.warning(f"Bing: Error in downloading {link}", exc_info=True)
+            LOGS.debug(f"Bing: Error in downloading {link}", exc_info=True)
 
     async def get_links(self):
         cached_urls = set()
@@ -86,7 +103,7 @@ class BingScrapper:
                     f"No more Image available for {self.query}. Page - {self.page_counter} | Downloaded - {len(cached_urls)}"
                 )
                 assert bool(cached_urls), f"Could not find any Images for {self.query}"
-                return self._evaluate(cached_urls)
+                return self._evaluate_links(cached_urls)
 
             img_links = findall("murl&quot;:&quot;(.*?)&quot;", response)
             for url in img_links:
@@ -94,9 +111,9 @@ class BingScrapper:
             self.page_counter += 1
 
         assert bool(cached_urls), f"Could not find any Images for {self.query}"
-        return self._evaluate(cached_urls)
+        return self._evaluate_links(cached_urls)
 
-    def _evaluate(self, links):
+    def _evaluate_links(self, links):
         links = list(links)
         shuffle(links)
         return links[: self.limit]
